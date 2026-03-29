009931d365c84bc6756b0a347b7940ca607d6368
vladislavkovnatskiy <i@vvkov.ru>
2026-03-29 22:36:44 +0300

    Moved the method recverceLetter to a new class StringUtill in the package com.example.util

--- src/ReverseLetter.java
+++ src/ReverseLetter.java
@@ -1,33 +1,10 @@
+import com.example.util.StringUtill;
+
 public class ReverseLetter {
     public static void main(String[] args) {
         String strInput = "J@va the be$t!123";
-        String strOutput = reverseLetter(strInput);
+        String strOutput = StringUtill.reverseLetter(strInput);
         System.out.println(strOutput);
     }
-    public static String reverseLetter(String str) {
-        if (str == null || str.isEmpty()) {
-            return "";
-        }
-        char[] output = new char[str.length()];
-        int count = str.length()-1;
-        for (int i = 0; i <= count; i++) {
-            if((str.charAt(i)>= 65 && str.charAt(i)<= 90)||(str.charAt(i)>= 97 && str.charAt(i)<= 122)){
-                //поиск буквы с конца
-                for(; count > i; count--) {
-                    if((str.charAt(count)>= 65 && str.charAt(count)<= 90)||(str.charAt(count)>= 97 && str.charAt(count)<= 122)){
-                        break;
-                    }
-                    //Если не буква, сохраняем на такую же позицию в массив вывода
-                    output[count] =  str.charAt(count);
-                }
-                char temp = str.charAt(i);
-                output[i] = str.charAt(count);
-                output[count] = temp;
-                count--;
-            } else  {
-                output[i] = str.charAt(i);
-            }
-        }
-        return new String(output);
-    }
+
 }
